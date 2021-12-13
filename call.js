af01a35a1bd690366ee6236d53f37faa5b4577ab
Dylan <1377927497@qq.com>
2021-12-13 11:45:52 +0800

    call,apply

---
+++ call.js
@@ -0,0 +1,31 @@
+Function.prototype.myCall = function (context) {
+  /**
+   * 实际上就是 将函数挂载到传入的对象中 执行
+   */
+
+  // 指定为 null 和 undefined 的 this 值会自动指向全局对象(浏览器中就是 window 对象)
+  // 值为原始值(数字，字符串，布尔值)的 this 会指向该原始值的自动包装对象(用 Object() 转换）
+  context = context ? Object(context) : window;
+
+  context.fn = this;
+
+  // 执行该函数
+  const args = [...arguments].slice(1);
+  const result = context.fn(args);
+
+  // 删除该函数
+  delete context.fn;
+
+  return result;
+};
+
+
+function sayWord() {
+  var talk = [this.name, "say", this.word].join(" ");
+  console.log(talk);
+}
+var bottle = {
+  name: "bottle",
+  word: "hello",
+};
+sayWord.myCall(bottle);
--- 防抖.js
+++ 防抖.js
@@ -1,9 +1,14 @@
 function debounce(func, wait) {
   let timeout;
   return function () {
+    console.log("this", this);
     const context = this;
     const args = arguments;
     timeout = null;
     timeout = setTimeout(() => func.apply(context, args), wait);
   };
 }
+
+const betterFn = debounce(() => console.log("fn 防抖执行了"), 2000);
+
+betterFn();
